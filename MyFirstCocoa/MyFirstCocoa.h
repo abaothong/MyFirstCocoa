ecdb560f76f8db682cf13dce552c5f8dd2162f7b
Thong Hao Yi <hy.thong@elken.com>
2019-02-25 15:04:13 +0800

    First Commit

---
+++ MyFirstCocoa/MyFirstCocoa.h
@@ -0,0 +1,19 @@
+//
+//  MyFirstCocoa.h
+//  MyFirstCocoa
+//
+//  Created by Thong Hao Yi on 25/02/2019.
+//  Copyright © 2019 Thong Hao Yi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyFirstCocoa.
+FOUNDATION_EXPORT double MyFirstCocoaVersionNumber;
+
+//! Project version string for MyFirstCocoa.
+FOUNDATION_EXPORT const unsigned char MyFirstCocoaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFirstCocoa/PublicHeader.h>
+
+
